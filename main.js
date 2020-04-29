0a65ea7e616aa7b0758504d3d98bb7e97c5b99e1
Tuomas Puro <tuomas.puro@esedu.fi>
2020-04-29 09:55:58 +0300

    Pieni esimerkki nimen tarkistuksesta

--- main.js
+++ main.js
@@ -2,3 +2,30 @@ function Submit() {
     document.getElementById("username").value
     document.getElementById("test").innerHTML = username
 }
+
+function checkForm(e){
+    e.preventDefault();
+    console.log(e)
+
+    let errors = [];
+
+    let uName = document.getElementById('username').value;
+
+    if (checkUserName(uName) == false) {
+        errors.push('Username is too short!');
+        document.getElementById('userNameError').innerHTML = 'Username is too short!';
+    }
+
+
+
+    console.log(errors);
+
+}
+
+function checkUserName(username){
+    if (username.length >= 4) {
+        return true;        
+    } else {
+        return false;
+    }
+}
