697bc5a7e0a7e91ed1cc0ec69864ee7793378efa
Hietzu <veeti.hietanen03@gmail.com>
2020-05-18 12:25:53 +0300

    Added final errors and completion notice

--- main.js
+++ main.js
@@ -8,25 +8,70 @@ function checkForm(e){
     let pWord = document.getElementById('password').value;
     let country = document.getElementById('country').value;
     let zip = document.getElementById('zipcode').value;
+    let email = document.getElementById('email').value;
+    let fName = document.getElementById('name').value
+    let fAddress = document.getElementById('address').value
 
     if (checkUserName(uName) == false) {
         errors.push('Username is too short');
         document.getElementById('userNameError').innerHTML = 'Username is too short!';
+    } else {
+        document.getElementById('userNameError').innerHTML = '';
     }
 
     if (checkPassWord(pWord) == false) {
         errors.push('Password is too short');
         document.getElementById('passWordError').innerHTML = 'Password is too short!';
+    } else {
+        document.getElementById('passWordError').innerHTML = '';
     }
 
     if (checkCountry(country) == false) {
         errors.push('Please select a country');
         document.getElementById('countryError').innerHTML = 'Please select a country!';
+    } else {
+        document.getElementById('countryError').innerHTML = '';
     }
 
+    
     if (checkZipCode(zip) == false) {
         errors.push('Zipcode has to be 5 digits long');
         document.getElementById('zipError').innerHTML = 'Zipcode has to be 5 digits long!';
+    } else {
+        document.getElementById('zipError').innerHTML = '';
+    }
+
+    if (checkEmail(email) == false) {
+        errors.push('Not a valid email');
+        document.getElementById('emailError').innerHTML = 'Not a valid email';
+    } else {
+        document.getElementById('emailError').innerHTML = '';
+    }
+
+    if (checkFullName(fName) == false) {
+        errors.push('Please enter your full name');
+        document.getElementById('nameError').innerHTML = 'Please enter your full name';
+    } else {
+        document.getElementById('nameError').innerHTML = '';
+    }
+
+    if (checkAddress(fAddress) == false) {
+        errors.push('Please enter your address');
+        document.getElementById('addressError').innerHTML = 'Please enter your address';
+    } else {
+        document.getElementById('addressError').innerHTML = '';
+    }
+
+    if (checkGender() == false) {
+        errors.push('Please select a gender');
+        document.getElementById('genderError').innerHTML = 'Please select a gender';
+    } else {
+        document.getElementById('genderError').innerHTML = '';
+    }
+
+    if (errors.length == 0) {
+        alert('Registeration complete. Redirecting...')
+        window.location.replace('http://it.esedu.fi/~hietanen.veeti/Hietzu22_homepage/En/index.html')
     }
 }
 
@@ -55,9 +100,55 @@ function checkCountry(country){
 }
 
 function checkZipCode(zip){
-    if (zip == 5) {
+    if (zip.length == 5) {
         return true;
     } else {
         return false
     }
+}
+
+function checkEmail(email){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkFullName(fName){
+    if (/^[a-öA-Ö]+ [a-öA-Ö]+$/.test(fName)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkAddress(fAddress){
+    if (/^[a-öA-Ö0-9\s-]{5,100}$/.test(fAddress)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkGender(){
+    if (document.getElementById('male').checked) {
+        return true;
+    } else if (document.getElementById('female').checked) {
+        return true;
+    } else if (document.getElementById('other').checked) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkRadios(x){
+    let rbs = document.querySelectorAll('input[name="'+x+'"]');
+    for (let i=0; i<rbs.length; i++){
+        if(rbs[i].checked == true){
+            return true;
+        }
+    }
+    return false;
 }
